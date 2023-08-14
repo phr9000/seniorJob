d2c4f46034f3a162baa66af049feb4be7e3e282d
ellie <gyeongjin.hong@dailyh.org>
2023-08-14 09:37:50 +0900

    + test commit!

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+// 파일경로 미생성 으로 인한 임시 주석처리
+// import Header from "../components/Header";
 
 const Root: React.FC = () => {
   return (
     <div>
-      <Header />
+      {/*<Header />*/}
       <div>
         <Outlet />
       </div>
