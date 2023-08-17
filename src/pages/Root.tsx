bb34b049f884cc2f4592f23f1a26bdaca8b0382d
ellie <gyeongjin.hong@dailyh.org>
2023-08-17 20:26:12 +0900

    + node sass delete and install dart sass

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import "../assets/styles/index.scss";
 // 파일경로 미생성 으로 인한 임시 주석처리
-import Header from "../components/Header";
+// import Header from "../components/Header";
 
 const Root: React.FC = () => {
   return (
     <div>
-      <Header />
+      {/*<Header />*/}
       <div>
-        <Outlet />
+        <Outlet/>
       </div>
     </div>
   );
