4c055cd2cd7f13bbe1320d54f76b0141d19143d1
ellie <gyeongjin.hong@dailyh.org>
2023-08-23 01:28:17 +0900

    + Section title defined in guide.tsx

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -1,7 +1,38 @@
+/* eslint-disable */
 import {Link} from "react-router-dom";
 import React from "react";
 
+
+export function SecTitle() {
+  return (
+    <h2 className="sec_title">Latest Jobs</h2>
+  );
+}
+
 const Guide: React.FC = () => {
-  return <>guide</>;
+  const commonStyle = {
+    backgroundColor: 'yellow',
+    marginTop: '20px',
+    marginBottom: '20px',
+  }
+  const fontSize30 = {
+    fontSize: '30px',
+    textIndent: '30px'
+  }
+  const fontSize20 = {
+    fontSize: '20px',
+    backgroundColor: 'lightPink',
+    textIndent: '60px'
+  }
+
+  return (
+    <>
+      <div style={{...commonStyle, ...fontSize30}}>Guide Page</div>
+      <div style={{...commonStyle, ...fontSize20}}>Section Titile</div>
+      <SecTitle/>
+
+
+    </>
+  );
 };
 export default Guide;
