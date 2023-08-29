3c499e82a57cbd6319bb3a6f284d1a53adf9bdcd
hailan <phr9000@gmail.com>
2023-08-29 16:52:13 +0900

    need help 중간

--- src/pages/Main/MainLayout.tsx
+++ src/pages/Main/MainLayout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 /* Import Component */
 // import LatestList from 'src/components/LatestList'
-import NeedHelp from './NeedHelp'
+import NeedHelp from './needhelp/NeedHelp'
 
 const MainLayout: React.FC = () => {
   return (
---
+++ src/pages/Main/needhelp/NeedHelp.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from "react";
+import Category from "./pannelview/Category";
+
+const LatestList: React.FC = () => {
+  return (
+
+    <section className="need-help-container">
+        <h2>Need help in finding the job you want?</h2>
+        <Category/>
+    </section>
+  );
+};
+export default LatestList;
