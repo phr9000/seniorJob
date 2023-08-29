10ebbd8674d63c468444d7897533c7fab4c71ad6
hailan <phr9000@gmail.com>
2023-08-29 20:26:17 +0900

    중간푸시

--- src/pages/Main/needhelp/pannelview/Category.tsx
+++ src/pages/Main/needhelp/pannelview/Category.tsx
@@ -4,18 +4,18 @@ import React from "react";
 const Category: React.FC = () => {
   return (
     <ul className="grid">
-        <li>
+      <li>
         <p className="img-area img-01"></p>
         <div className="txt-area flex items-center justify-between">
-            <p>
-                <h3>txt01</h3>
-                <span>txt02</span>
-            </p>
-            <p className="arrow-area">
-                <i className="fa-solid fa-arrow-right"></i>
-            </p>
+          <p>
+            <h3>txt01</h3>
+            <span>txt02</span>
+          </p>
+          <p className="arrow-area">
+            <i className="fa-solid fa-arrow-right"></i>
+          </p>
         </div>
-        </li>
+      </li>
     </ul>
   );
 };
