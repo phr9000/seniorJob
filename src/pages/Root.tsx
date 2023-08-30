f39a7c54d3ba1e2cd9c63fa95ea2db3667d440d0
hong <hkj2855@gmail.com>
2023-08-31 01:01:37 +0900

    + 상대경로 링크를 절대경로로 통일화 수정

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import "../assets/styles/index.scss";
+import "src/assets/styles/index.scss";
 import "src/assets/styles/index.scss";
 import "src/assets/fontawesome/css/fontawesome.css";
 import "src/assets/fontawesome/css/brands.css";
 import "src/assets/fontawesome/css/solid.css";
-// 파일경로 미생성 으로 인한 임시 주석처리
-import Header from "../components/Header";
+
+import Header from "src/components/Header";
+
+
+
 
 const Root: React.FC = () => {
   return (
