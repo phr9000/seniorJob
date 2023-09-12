6aae49924ed015790acf724113869eff6958ac6f
hailan <phr9000@gmail.com>
2023-09-12 09:15:11 +0900

    serach responsive

--- src/pages/Main/searchbox/SearchBox.tsx
+++ src/pages/Main/searchbox/SearchBox.tsx
@@ -1,3 +1,4 @@
+// serachbox
 /* eslint-disable */
 import React, { useState } from "react";
 import Input from "src/components/input/TextInput";
@@ -16,38 +17,40 @@ const SearchBox: React.FC = () => {
 
   return (
     <>
-      <div className="top flex">
-        <div className="col-xs-12 col-sm-6 col-md-6">
-          <Input placeholder="Job Title, Company Name, Keyword" />
+      <div className="search-box-inner">
+        <div className="top flex">
+          <div className="col-xs-12 col-sm-6 col-md-6">
+            <Input placeholder="Job Title, Company Name, Keyword" />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-2">
+            <SelectBoxChk
+              isOpen={isDropdownOpen1}
+              toggleDropdown={() => setIsDropdownOpen1(!isDropdownOpen1)}
+              selectedItems={selectedItems1}
+              items={["All", "Option1", "Option2"]} // 적절한 아이템 배열 전달
+              defaultLabel="Category"
+            />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-2">
+            <SelectBoxChk
+              isOpen={isDropdownOpen2}
+              toggleDropdown={() => setIsDropdownOpen2(!isDropdownOpen2)}
+              selectedItems={selectedItems2}
+              items={["All", "Option1", "Option2"]} // 적절한 아이템 배열 전달
+              defaultLabel="Emplyment Type"
+            />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-2">
+            <SelectBoxBasic />
+          </div>
         </div>
-        <div className="col-xs-12 col-sm-6 col-md-2">
-          <SelectBoxChk
-            isOpen={isDropdownOpen1}
-            toggleDropdown={() => setIsDropdownOpen1(!isDropdownOpen1)}
-            selectedItems={selectedItems1}
-            items={["All", "Option1", "Option2"]} // 적절한 아이템 배열 전달
-            defaultLabel="Category"
-          />
-        </div>
-        <div className="col-xs-12 col-sm-6 col-md-2">
-          <SelectBoxChk
-            isOpen={isDropdownOpen2}
-            toggleDropdown={() => setIsDropdownOpen2(!isDropdownOpen2)}
-            selectedItems={selectedItems2}
-            items={["All", "Option1", "Option2"]} // 적절한 아이템 배열 전달
-            defaultLabel="Emplyment Type"
-          />
-        </div>
-        <div className="col-xs-12 col-sm-6 col-md-2">
-          <SelectBoxBasic />
-        </div>
+        <Button
+          color="primary"
+          size="full"
+          label="&nbsp;&nbsp;Search Job"
+          icon="fa-solid fa-magnifying-glass"
+        />
       </div>
-      <Button
-        color="primary"
-        size="full"
-        label="&nbsp;&nbsp;Search Job"
-        icon="fa-solid fa-magnifying-glass"
-      />
     </>
   );
 };
