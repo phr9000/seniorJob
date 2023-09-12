42bd834bab6a2b6bce0963f4613477b965fe8901
hailan <phr9000@gmail.com>
2023-09-12 17:00:17 +0900

    jobs revise

--- src/pages/Jobs/JobsLayout.tsx
+++ src/pages/Jobs/JobsLayout.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable */
 import React, { useEffect, useState, useRef } from "react";
-
+import LatestList from "src/components/LatestList";
 /* Import Component */
 const JobsLayout: React.FC = () => {
     return (
-        <main className="main-container">
+        <main className="main-container jobs-layout-container">
             <section className="container">
                 <h2 className="text-left">All Jobs</h2>
+                <LatestList />
             </section>
         </main>
     );
