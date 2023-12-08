8bfed6c340f040d64a72ec8f980711f86f8240be
hailan <phr9000@gmail.com>
2023-12-08 15:24:50 +0900

    mind 주석

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,42 @@
 /* eslint-disable */
-import {NavLink} from "react-router-dom";
-import React, {useState} from "react";
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
 import Button from "src/components/button/Button";
 import ResumePopup from "src/components/popup/resumePopup";
 import LoginSignupPopup from "src/components/popup/LoginSignupPopup";
 
-
-
-
 const Header: React.FC = () => {
   // dropdown
-  const [ isMenuOpen, setIsMenuOpen ] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const handleClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-  
+
   // NavLink 클릭 시 메뉴 닫기 이벤트
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
-  
+
   //시작 Resume Popup
-  const [ isResumePopupOpen, setIsResumePopupOpen ] = useState(false);
-  
+  const [isResumePopupOpen, setIsResumePopupOpen] = useState(false);
+
   const ResumeOpenPopup = () => {
     setIsResumePopupOpen(true);
   };
-  
+
   const closePopup = () => {
     setIsResumePopupOpen(false);
     setIsLoginPopupOpen(false);
   };
   //끝 Resume Popup
-  
+
   //시작 Login Popup
-  const [ isLoginPopupOpen, setIsLoginPopupOpen ] = useState(false);
+  const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
   const LoginOpenPopup = () => {
     setIsLoginPopupOpen(true);
   };
   //끝 Login Popup
-  
+
   return (
     <>
       <header>
@@ -54,7 +51,7 @@ const Header: React.FC = () => {
               <li className="active h-100">
                 <NavLink
                   to="/"
-                  className={({isActive}) => ( isActive ? "active" : "" )}
+                  className={({ isActive }) => (isActive ? "active" : "")}
                   onClick={closeMenu}
                 >
                   Home
@@ -63,7 +60,7 @@ const Header: React.FC = () => {
               <li>
                 <NavLink
                   to="/jobs"
-                  className={({isActive}) => ( isActive ? "active" : "" )}
+                  className={({ isActive }) => (isActive ? "active" : "")}
                   onClick={closeMenu}
                 >
                   Jobs
@@ -72,7 +69,7 @@ const Header: React.FC = () => {
               <li>
                 <NavLink
                   to="/Courses"
-                  className={({isActive}) => ( isActive ? "active" : "" )}
+                  className={({ isActive }) => (isActive ? "active" : "")}
                   onClick={closeMenu}
                 >
                   Courses
@@ -81,13 +78,13 @@ const Header: React.FC = () => {
               <li>
                 <NavLink
                   to="/ContactUs"
-                  className={({isActive}) => ( isActive ? "active" : "" )}
+                  className={({ isActive }) => (isActive ? "active" : "")}
                   onClick={closeMenu}
                 >
                   Contact Us
                 </NavLink>
               </li>
-              <li>
+              {/* <li>
               <NavLink
                   to="/mind"
                   className={({isActive}) => ( isActive ? "active" : "" )}
@@ -95,7 +92,7 @@ const Header: React.FC = () => {
                 >
                   mind
                 </NavLink>
-              </li>
+              </li> */}
             </ul>
             <Button
               label="Resume Drop Off"
@@ -103,14 +100,14 @@ const Header: React.FC = () => {
               additionalClass="ml-6"
               onClick={ResumeOpenPopup}
             />
-            <Button label="Search Candidate" additionalClass="ml-8"/>
+            <Button label="Search Candidate" additionalClass="ml-8" />
             <Button
               label="Login / Sign Up"
               color="primary-fill"
               additionalClass="ml-8"
               onClick={LoginOpenPopup}
             />
-            <Button label="KO/EN" color="red-fill" additionalClass="ml-8"/>
+            <Button label="KO/EN" color="red-fill" additionalClass="ml-8" />
           </div>
           <div className="menubar-icon" onClick={handleClick}>
             <i
@@ -121,8 +118,8 @@ const Header: React.FC = () => {
           </div>
         </div>
       </header>
-      <ResumePopup isPopup={isResumePopupOpen} closePopup={closePopup}/>
-      <LoginSignupPopup isPopup={isLoginPopupOpen} closePopup={closePopup}/>
+      <ResumePopup isPopup={isResumePopupOpen} closePopup={closePopup} />
+      <LoginSignupPopup isPopup={isLoginPopupOpen} closePopup={closePopup} />
     </>
   );
 };
